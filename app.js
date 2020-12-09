1f309c6f508accf18fd7ef6fca28e395249670fc
Ramon <ramon.s.v@hotmail.com>
2020-12-09 19:58:49 -0300

    Adicionado rotas de backend, criado mais paginas de frontend, modulos de comunicação com o banco de dados adicionados, correções em algumas páginas e mudanças de estilos.

--- app.js
+++ app.js
@@ -6,7 +6,17 @@ const bodyParser = require("body-parser");
 // Config Handlebars
 
 app.set("views", __dirname + "/views");
-app.engine("hbs", handlebars({ defaultLayout: "main", extname: ".hbs" }));
+app.engine(
+  "hbs",
+  handlebars({
+    defaultLayout: "main",
+    extname: ".hbs",
+    runtimeOptions: {
+      allowProtoPropertiesByDefault: true,
+      allowProtoMethodsByDefault: true,
+    },
+  })
+);
 app.set("view engine", "hbs");
 
 // Config bodyParser
@@ -25,15 +35,70 @@ app.get("/", function (req, res) {
 });
 
 app.get("/dashboard", function (req, res) {
-  res.render("dashboard");
+  chamados.findAll().then(function (chamados) {
+    res.render("dashboard", { chamados: chamados });
+  });
 });
 
-app.get("/cadastro", function (req, res) {
-  res.render("cadastro");
+app.get("/criarCadastro", function (req, res) {
+  res.render("criarCadastro");
+});
+
+app.get("/criarsalas", function (req, res) {
+  res.render("criarSalas");
 });
 
 app.get("/criarChamado", function (req, res) {
-  res.render("criarChamado");
+  salas.findAll().then(function (salas) {
+    res.render("criarChamado", { salas: salas });
+  });
 });
 
-app.listen(8081);
+const db = require("./models/db");
+const { salas, chamados } = require("./models/db");
+
+app.post("/addChamado", function (req, res) {
+  db.chamados
+    .create({
+      nome: req.body.nome,
+      salas: req.body.salas,
+      problema: req.body.problema,
+      idUser: req.body.idUser,
+    })
+    .then(function () {
+      res.redirect("/criarChamado");
+    })
+    .catch(function () {
+      res.redirect("/criarChamado");
+    });
+});
+
+app.post("/addSalas", function (req, res) {
+  db.salas
+    .create({
+      nome: req.body.nome,
+    })
+    .then(function () {
+      res.redirect("/criarSalas");
+    })
+    .catch(function () {
+      res.redirect("/criarSalas");
+    });
+});
+
+app.post("/addCadastro", function (req, res) {
+  db.usuarios
+    .create({
+      nome: req.body.nome,
+      cpf: req.body.cpf,
+      cargo: req.body.cargo,
+    })
+    .then(function () {
+      res.redirect("/criarCadastro");
+    })
+    .catch(function (err) {
+      res.redirect("/criarCadastro");
+    });
+});
+
+app.listen(3000);
